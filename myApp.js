5240708e6a9966f6ae2022c8a41fb2e1e6c09d94
Jottace Caria <julio.caria@hotmail.com>
2024-06-30 14:18:37 +0000

    feat: add file with response

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+// console.log("Hello World")
 
-
+app.get('/', (req, res) => {
+    absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
+})
 
 
 
