1b1237a3397be1c199f10a2513f48e76819f7ddb
Jottace Caria <julio.caria@hotmail.com>
2024-06-30 14:46:14 +0000

    feat: add json route

--- myApp.js
+++ myApp.js
@@ -13,6 +13,9 @@ app.get('/', (req, res) => {
     app.use("/public", express.static(__dirname + "/public"));
 })
 
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"})
+})
 
 
 
